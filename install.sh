5d87d5dfe247b8f089adbe840fa2fa5e5c16663c
Cuong Duy Nguyen <103cuong@gmail.com>
2020-07-19 17:17:06 +0700

    fix: blacklist (#3)

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo '###---------- free-spotify ----------###' >> /etc/hosts
-curl https://raw.githubusercontent.com/x0uid/SpotifyAdBlock/master/SpotifyBlocklist.txt >> /etc/hosts
+curl https://raw.githubusercontent.com/x0uid/SpotifyAdBlock/master/hosts >> /etc/hosts
 echo '###---------- made by @103cuong ----------###' >> /etc/hosts
