fe804e50ca8fbfc35bd62a7fd239f2b9cb646402
Cuong Duy Nguyen <103cuong@gmail.com>
2020-07-19 15:12:40 +0700

    🎉 first version: add install.sh & uninstall.py (#1)

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo '###---------- free-spotify ----------###' >> /etc/hosts
+curl https://raw.githubusercontent.com/x0uid/SpotifyAdBlock/master/SpotifyBlocklist.txt >> /etc/hosts
+echo '###---------- made by 103cuong ----------###' >> /etc/hosts
