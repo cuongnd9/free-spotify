3682ddcc0f242457beba9ddaea5e6ff28b3dc53f
Cuong Duy Nguyen <103cuong@gmail.com>
2020-07-19 15:31:50 +0700

    chore: remove new line after uninstall app

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 echo '###---------- free-spotify ----------###' >> /etc/hosts
 curl https://raw.githubusercontent.com/x0uid/SpotifyAdBlock/master/SpotifyBlocklist.txt >> /etc/hosts
-echo '###---------- made by 103cuong ----------###' >> /etc/hosts
+echo '###---------- made by @103cuong ----------###' >> /etc/hosts
