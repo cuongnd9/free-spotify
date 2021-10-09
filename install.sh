99fefb24207440dca2feefaa789246afc2a74161
H & H <harrytran103@gmail.com>
2021-10-09 13:55:21 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 echo '###---------- free-spotify ----------###' >> /etc/hosts
 curl https://raw.githubusercontent.com/x0uid/SpotifyAdBlock/master/hosts >> /etc/hosts
-echo '###---------- made by @kimcuong060498 ----------###' >> /etc/hosts
+echo '###---------- made by @harrytran103 ----------###' >> /etc/hosts
